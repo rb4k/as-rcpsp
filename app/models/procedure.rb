e2a8a4992aa5776eb706fafb82151bd389a06679
Andreas Hipp <andihipp@web.de>
2015-02-21 20:29:06 +0100

    kaputterere Topologie

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,8 +1,9 @@
 class Procedure < ActiveRecord::Base
   attr_accessible :created_at, :kapabe, :name, :prot, :updated_at
 
+
+  has_many :procedure_users, :dependent => :destroy
   has_many :topologics, foreign_key: "prepro_id", :dependent => destroy
   has_many :reverse_topologics, foreign_key: "sucpro_id", class_name: "Topologic", :dependent => :destroy
-  has_many :procedure_users, :dependent => :destroy
 
 end
--- app/models/topologic.rb
+++ app/models/topologic.rb
@@ -1,5 +1,6 @@
 class Topologic < ActiveRecord::Base
   attr_accessible :prepro_id, :sucpro_id
+  validates :numericality => {greater_than => 0}, :presence => true
 
   belongs_to :prepro, class_name: "Procedure"
   belongs_to :sucpro, class_name: "Procedure"
