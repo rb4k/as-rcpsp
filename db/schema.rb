6ceac044d83fe7206cbb327d942d7238d4a37cd6
Robert Matern <rob4ik@yahoo.de>
2015-02-23 19:19:49 +0100

    Jeder seine Eistellung

--- db/schema.rb
+++ db/schema.rb
@@ -13,29 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20150220172822) do
 
-  create_table "demandsites", :force => true do |t|
-    t.integer  "site_id"
-    t.float    "demand_quantity"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "machine_periods", :force => true do |t|
-    t.integer  "machine_id"
-    t.integer  "period_id"
-    t.float    "capacity"
-    t.float    "overtime"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "machines", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.float    "overtime_cost"
-  end
-
   create_table "periods", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -62,67 +39,6 @@ ActiveRecord::Schema.define(:version => 20150220172822) do
     t.datetime "updated_at", :null => false
     t.string   "kapabe"
     t.string   "prot"
-  end
-
-  create_table "product_machines", :force => true do |t|
-    t.integer  "product_id"
-    t.integer  "machine_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "product_periods", :force => true do |t|
-    t.integer  "product_id"
-    t.integer  "period_id"
-    t.float    "demand"
-    t.float    "production"
-    t.float    "inventory"
-    t.integer  "setup"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "product_products", :force => true do |t|
-    t.integer  "from_product_id"
-    t.integer  "to_product_id"
-    t.integer  "coefficient"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "products", :force => true do |t|
-    t.string   "name"
-    t.float    "setup_time"
-    t.float    "processing_time"
-    t.float    "setup_cost"
-    t.float    "holding_cost"
-    t.float    "initial_inventory"
-    t.integer  "lead_time_periods"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-  end
-
-  create_table "sites", :force => true do |t|
-    t.string   "name"
-    t.string   "codename",   :limit => 3
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-  end
-
-  create_table "supplysites", :force => true do |t|
-    t.integer  "site_id"
-    t.float    "supply_quantity"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "translinks", :force => true do |t|
-    t.integer  "supplysite_id"
-    t.integer  "demandsite_id"
-    t.float    "unit_cost"
-    t.float    "transport_quantity"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
   end
 
   create_table "users", :force => true do |t|
