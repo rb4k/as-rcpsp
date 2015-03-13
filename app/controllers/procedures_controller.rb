0ef393146c86651ec3c88ba598678a058c4bd9eb
Robert Matern <rob4ik@yahoo.de>
2015-03-13 23:33:55 +0100

    So toll

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -30,6 +30,9 @@ class ProceduresController < ApplicationController
       format.json { render json: @procedure }
     end
   end
+
+
+
 # GET /procedures/1/edit
   def edit
     @procedure = Procedure.find(params[:id])
